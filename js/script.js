5feb80e8b0ff314aec0b2120e2d81bc5a749f6c4
YeasinSharif <mahfujakash2@gmail.com>
2022-04-09 15:13:16 +0600

    food

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+const navbarToggler = document.querySelector('.navbar-toggler');
+navbarToggler.addEventListener('click', () => {
+    navbarToggler.classList.toggle('change-icon');
+    document.querySelector('.navbar-collapse').classList.toggle('show-navbar');
+});
+
+
